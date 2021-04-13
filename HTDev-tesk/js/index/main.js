6890862bf60a31947c83f66e767e95e67c9f2b43
Саша <49306587+scythecult@users.noreply.github.com>
2021-04-13 12:24:26 +0300

    Add files via upload

---
+++ HTDev-tesk/js/index/main.js
@@ -0,0 +1,32 @@
+new Swiper(".slider.slider--works", {
+	navigation: {
+		nextEl: ".projects .swiper-button-next",
+		prevEl: ".projects .swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
+
+new Swiper(".slider.slider--principles", {
+	navigation: {
+		nextEl: ".principles .swiper-button-next",
+		prevEl: ".principles .swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
+
+new Swiper(".slider.slider--employees", {
+	navigation: {
+		nextEl: ".employees .swiper-button-next",
+		prevEl: ".employees .swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
